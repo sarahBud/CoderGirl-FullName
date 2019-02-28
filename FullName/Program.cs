e6187b4927ab7885191e76e3b788c8b620ce36a6
sebudwell@gmail.com <sebudwell@gmail.com>
2019-02-28 08:36:16 -0600

    Test Error
    Error CS0122 'Program.FullName(string, string)' is inaccessible due to its protection level Test C:\Users\Sarah Budwell\source\repos\sarahBud\CoderGirl-FullName\Test\ProgramTest.cs 73 Active

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -11,10 +11,27 @@ namespace FullName
             // TODO: Call a method called FullName that returns their full name.
 
             // TODO: Output their full name.
+            string firstName;
+            string lastName;
+
+            Console.WriteLine("What is your first name? ");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name? ");
+            lastName = Console.ReadLine();
+
+            //Console.WriteLine($"{firstName} {lastName}");
+            FullName(firstName, lastName);
 
             Console.ReadLine();
         }
+        static void FullName(string firstName, string lastName)
+        {
+       
 
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+        
         // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
     }
 }
